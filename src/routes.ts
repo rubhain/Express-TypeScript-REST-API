40f6d55307355f599bdd6e91b4016f5817ec0446
rubhain <bugrahantufekcioglu@gmail.com>
2022-09-08 00:07:25 +0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,31 @@
+import express, {Express, Request, Response} from "express";
+import { createUserSessionHandler, getUserSessionsHandler } from "./controller/session.controller";
+import { createUserHandler } from "./controller/user.controller";
+import requireUser from "./middleware/requireUser";
+import validateResource from "./middleware/validateResource";
+import { createSessionSchema } from "./schema/session.schema";
+import { createUserSchema } from "./schema/user.schema";
+
+
+function routes(app: Express){
+
+    app.get("/healthcheck", 
+    (req: Request, res: Response) => res.sendStatus(200));
+
+
+    app.post(
+        '/api/users', 
+        validateResource(createUserSchema), 
+        createUserHandler
+        );
+
+    app.post(
+        '/api/sessions', 
+        validateResource(createSessionSchema), 
+        createUserSessionHandler
+        );
+
+    app.get('/api/sessions', requireUser, getUserSessionsHandler);
+}
+
+export default routes;  
